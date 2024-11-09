ffe543d09536ac5d5b7bdae29286326b6659a2c9
PaulineVarin <47080793+PaulineVarin@users.noreply.github.com>
2024-11-09 17:41:35 +0100

    fix: add link to geodata

--- visu_pauline/dot_map.js
+++ visu_pauline/dot_map.js
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=c0a02WHjgEs
 //SVG definition
 var svg = d3.select("svg"),
     width = window.innerWidth -100 ,
@@ -31,22 +32,64 @@ d3.select('#zoom-out').on('click', function() {
 
 svg.call(zoom);
 
+d3.queue()
+  .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")  // dataGeo
+  .defer(d3.json, "../traitements_mongoDB/fichiers_json/UX_Projet.wasabi_clean_map.json") // data
+  .await(ready);
 
-// Load external data and boot
-d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", function(data){
+function ready(error, dataGeo, data) {
 
-    // Map and projection
-    var projection = d3.geoNaturalEarth1()
-    .fitSize([width, height], data);
+  const x = data
 
-    // Draw the map
-      g
-        .selectAll("path")
-        .data(data.features)
-        .enter().append("path")
-            .attr("fill", "#808080")
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .style("stroke", "#fff") 
-})
+  //Récupération des continents
+  var allContinent = d3.map(data, function(d){
+    return(d._id.pays)
+  }).keys()
+
+  //Mise en place des couleurs pour les continents
+  var color = d3.scaleOrdinal()
+    .domain(allContinent)
+    .range(d3.schemePaired);
+
+    // Add a scale for bubble size
+    var valueExtent = d3.extent(data, function(d) { return +d.nbAlbums; })
+    var size = d3.scaleSqrt()
+      .domain(valueExtent)  // What's in the data
+      .range([ 1, 50])  // Size in pixel
+
+  
+  // Map and projection
+  var projection = d3.geoNaturalEarth1()
+  .fitSize([width, height], dataGeo);
+
+  dataGeo.features.forEach(d => {
+    var countgenre = data.filter( item => item._id.pays == d.properties.name)
+    //traiter le cas ou le pays n'a pas de genre et voir pour la liaison à faire 
+    Object.assign(d.properties, {"listeCountGenre": countgenre})
+  });
+
+  console.log(dataGeo.features)
+
+  // Draw the map
+    g
+      .selectAll("path")
+      .data(dataGeo.features)
+      .enter().append("path")
+          .attr("fill", "#808080")
+          .attr("d", d3.geoPath()
+              .projection(projection)
+          )
+          .style("stroke", "#fff") 
+
+          console.log(dataGeo.features)
+  // Add circles
+    g
+      .selectAll("circle")
+      .data(dataGeo.features)
+      .enter().append('circle')
+      .attr('class', 'country-circle')
+      .attr('cx', d=> projection(d3.geoCentroid(d))[0])
+      .attr('cy', d=> projection(d3.geoCentroid(d))[1])
+      .attr('r',function(d){ return size(+d.properties.listeCountGenre[0].nbAlbums)})
+}
+
