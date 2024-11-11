cf187bb0c0bda0c3b7dc22f596a087a863ec9c88
PaulineVarin <47080793+PaulineVarin@users.noreply.github.com>
2024-11-11 16:44:43 +0100

    fix: add click event + infos sur la bulle

--- visu_pauline/dot_map.js
+++ visu_pauline/dot_map.js
@@ -1,4 +1,6 @@
 //https://www.youtube.com/watch?v=c0a02WHjgEs
+var genreAGarder = "Latin"
+
 //SVG definition
 var svg = d3.select("svg"),
     width = window.innerWidth -100 ,
@@ -6,7 +8,6 @@ var svg = d3.select("svg"),
 
 svg.attr('width', width).attr('height', height);
 var g = svg.append("g");
-
 
 //Zoom definition
 function zoomed() {
@@ -20,76 +21,113 @@ var zoom = d3.zoom()
     .translateExtent([[0, 0], [width, height]]) //limit the zoom at the map
     .on("zoom", zoomed);
 
+
 d3.select('#zoom-in').on('click', function() {
   // Smooth zooming
-	zoom.scaleBy(svg.transition().duration(750), 1.3);
+  zoom.scaleBy(svg.transition().duration(750), 1.3);
 });
 
 d3.select('#zoom-out').on('click', function() {
   // Ordinal zooming
   zoom.scaleBy(svg, 1 / 1.3);
 });
+    
+svg.call(zoom)
 
-svg.call(zoom);
 
+//Tooltip definition
+var Tooltip = d3.select("#my_dataviz")
+.append("div")
+.attr("class", "tooltip")
+.style("opacity", 1)
+.style("background-color", "white")
+.style("border", "solid")
+.style("border-width", "2px")
+.style("border-radius", "5px")
+.style("padding", "5px")
+.attr('style', 'position: absolute;');
+
+//Mouse Action definition
+var mouseover = function(d) {
+  Tooltip.style("opacity", 1)
+}
+var mousemove = function(d) {
+  Tooltip
+    .html(d.properties.listeCountGenre[0]._id.pays + "<br>" + "Nombre d'albums: "+ "<br>" + d.properties.listeCountGenre[0].nbAlbums)
+    .style("left", (30+ "px"))
+    .style("top", (30 + "px"))
+}
+var mouseleave = function(d) {
+  Tooltip.style("opacity", 0)
+}
+
+var mouseclick = function(d) {
+  console.log("Hello " + d.properties.listeCountGenre[0]._id.pays)
+}
+
+
+//Chargement des donnees pour la carte et wasabi
 d3.queue()
   .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")  // dataGeo
   .defer(d3.json, "../traitements_mongoDB/fichiers_json/UX_Projet.wasabi_clean_map.json") // data
   .await(ready);
 
 function ready(error, dataGeo, data) {
+  //Filtrage des donnees
+  data = data.filter(item => item._id.genreP == genreAGarder)
 
-  const x = data
-
-  //Récupération des continents
-  var allContinent = d3.map(data, function(d){
-    return(d._id.pays)
-  }).keys()
-
-  //Mise en place des couleurs pour les continents
-  var color = d3.scaleOrdinal()
-    .domain(allContinent)
-    .range(d3.schemePaired);
-
-    // Add a scale for bubble size
-    var valueExtent = d3.extent(data, function(d) { return +d.nbAlbums; })
-    var size = d3.scaleSqrt()
-      .domain(valueExtent)  // What's in the data
-      .range([ 1, 50])  // Size in pixel
+  // Mise en place du calcul de la taille des bulles
+  var valueExtent = d3.extent(data, function(d) { return +d.nbAlbums; })
+  var size = d3.scaleSqrt()
+    .domain(valueExtent)  // What's in the data
+    .range([0, 50])  // Size in pixel
 
   
-  // Map and projection
+  //Mise en place de la carte
   var projection = d3.geoNaturalEarth1()
-  .fitSize([width, height], dataGeo);
+  .fitSize([width, height], dataGeo)
 
+  //Liaison entre dataGeo et data sur le nom de pays
+  //voir pour la liaison à faire car code sur 2 dans mongo et sur geo sur 3
   dataGeo.features.forEach(d => {
-    var countgenre = data.filter( item => item._id.pays == d.properties.name)
-    //traiter le cas ou le pays n'a pas de genre et voir pour la liaison à faire 
-    Object.assign(d.properties, {"listeCountGenre": countgenre})
+    var listGenresWithCount = data.filter( item => item._id.pays == d.properties.name)
+    if(listGenresWithCount.length == 0) {
+      elem = {"nbAlbums" : 0}
+      listGenresWithCount.push(elem)
+    }
+
+    //Rajout d'une liste contenant un objet avec le nombre d'albums pour le pays
+    Object.assign(d.properties, {"listeCountGenre": listGenresWithCount})
   });
 
   console.log(dataGeo.features)
 
-  // Draw the map
+  // Affichage de la carte
     g
       .selectAll("path")
       .data(dataGeo.features)
       .enter().append("path")
-          .attr("fill", "#808080")
-          .attr("d", d3.geoPath()
-              .projection(projection)
-          )
-          .style("stroke", "#fff") 
+        .attr("fill", "#808080")
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+      .style("stroke", "#fff") 
 
-          console.log(dataGeo.features)
-  // Add circles
+  // Affichage des cercles
     g
-      .selectAll("circle")
+      .selectAll('circle')
       .data(dataGeo.features)
-      .enter().append('circle')
-      .attr('class', 'country-circle')
-      .attr('cx', d=> projection(d3.geoCentroid(d))[0])
-      .attr('cy', d=> projection(d3.geoCentroid(d))[1])
-      .attr('r',function(d){ return size(+d.properties.listeCountGenre[0].nbAlbums)})
+      .enter()
+      .append('circle')
+        .attr('class', 'circle')
+        .attr('cx', d=> projection(d3.geoCentroid(d))[0])
+        .attr('cy', d=> projection(d3.geoCentroid(d))[1])
+        .attr('r',function(d){ return size(+d.properties.listeCountGenre[0].nbAlbums)})
+        .attr('fill-opacity', 0.2)
+        .style("fill", "red")
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+      .on("click",mouseclick)
 }
 
